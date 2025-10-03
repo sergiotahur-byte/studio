f0bf384049690b99c6d6bdf2d3fc1937fb9a8d84
sergio alvarez <sergiotahur@gmail.com>
2025-10-03 02:31:35 +0000

    ya se corrigió el error? la página web está publicada?

--- src/components/sections/consultation-packages.tsx
+++ src/components/sections/consultation-packages.tsx
@@ -181,8 +181,7 @@ const CustomContractPackage = () => {
             <RadioGroupItem value="comercial" id="creation-comercial" className="peer sr-only" />
             <Label
               htmlFor="creation-comercial"
-              className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p
--4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
+              className="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-accent hover:text-accent-foreground peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary"
             >
               Comercial / Especial
             </Label>
@@ -266,3 +265,5 @@ export default function ConsultationPackages() {
     </section>
   );
 }
+
+    
