2a722d23270d15f6d3313ec89c42e16ebd747cc4
sergio alvarez <sergiotahur@gmail.com>
2025-10-04 03:06:38 +0000

    sigue siendo no válida. Estás seguro que no estamos atrapados en un bucl

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/gracias'], // No es necesario indexar la página de agradecimiento
+      disallow: '/gracias', // No es necesario indexar la página de agradecimiento
     },
     sitemap: 'https://www.recuperacionesjuridicas.lat/sitemap.xml',
   };
